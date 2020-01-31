bd38cffaae22e45424581a2c267a6fd9e94a546c
Oleg Kovasorov <okovasorov@xdsoft.ru>
2020-01-31 13:39:16 +0300

    initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+#print "Hello Again"
+#print "I like typong this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I's much rather you 'not'."
+#print 'I "said" do not touch this.'
+# print "Hello-heloo!"
---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+inp = raw_input("Print arguments: ")
+argv = inp
+
+print argv
+print "The scrip is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
